75e7175d617b92f516e8ef03d672a8afad5210b4
felipe <104036699+felipeduarte2@users.noreply.github.com>
2024-06-19 13:31:46 -0600

    Cambio de color del boton ver detalles

--- resources/views/director/dashboard.blade.php
+++ resources/views/director/dashboard.blade.php
@@ -68,15 +68,7 @@
                                         <td class="py-4 px-1 text-center">{{ $solicitud->Motivo }}</td>
                                         <td class="py-4 px-1 text-center">{{ date('d-m-Y', strtotime($solicitud->FechaSolicitada)). ' ' . date('H:i', strtotime($solicitud->HoraSolicitada)) }}</td>
                                         {{-- <td class="py-4 px-1 text-center">{{ $solicitud->Observaciones }}</td> --}}
-                                        <td class="py-4 px-1 text-center">
-                                            <a class="ml-4 inline-flex items-center px-4 py-2 bg-gray
-                                            -800 dark:bg-gray-200 border border-transparent rounded-md
-                                            font-semibold text-xs text-white dark:text-gray-800
-                                            uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white
-                                            focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900
-                                            dark:active:bg-gray-300 focus:outline-none focus:ring-2
-                                            focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800
-                                            transition ease-in-out duration-150"
+                                        <td class="py-4 px-1 text-center">class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                                             href="{{ route('director.detalles_solicitud_p', $solicitud->id)}}">Ver Detalles</a>
                                         </td>
                                     </tr>
